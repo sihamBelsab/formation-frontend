284a11004dfa8be41bb43469d05f2a8f451e40f0
sihamBelsab <sihambelaid945@gmail.com>
2025-06-21 15:01:57 +0000

    Update Header.jsx

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './header.css';
-import Logo from '../all/logo';
 import Search from '../all/Search';
 import Nav from './Nav';
+import Logo from '../all/Logo';
 function Header({ userInfo, setUserInfo }) {
   return (
     <header
       id='header'
       className='header relative fixed-top d-flex align-items-center justify-content-between'
     >
-      <Logo 
-      />
+      <Logo />
       <Search />
       <Nav userInfo={userInfo} setUserInfo={setUserInfo} />
     </header>
