fd1f7baa2d5cf74fb1fceafb2bcf7c2c2e8b4d2a
sihamBelsab <sihambelaid945@gmail.com>
2025-06-22 02:45:51 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    allowedHosts:true
-  }
+  server: {
+    allowedHosts: true,
+  },
+
+  rewrites: [{ source: '/(.*)', destination: '/' }],
 });
